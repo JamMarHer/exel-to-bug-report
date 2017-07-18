c25666802a46779349387137e55face1d46b56d8
Jam <jamarck96@gmail.com>
2017-07-18 17:15:36 -0400

    changed reportBugs to a more elegant apporach now taht we have the sheet nicely formated

--- excel_to_bug_report.py
+++ excel_to_bug_report.py
@@ -18,41 +18,26 @@ def processExcel(workbookInput):
 
 
 
-# Done this way since we don't have the sheet in correct format. Can be easily
-# improved after we have it nicely formated.
 def reportBugs(formatedData):
-    cellNotPopulated = 'Cell Not Populated'
+    with open ('format', 'r') as input_format:
+        _format = input_format.readlines()
     for commit in formatedData:
-        with open('bugs/{}.bug'.format(formatedData[commit][11]), 'a') as bugReport:
-            bugReport.write('title: {}\n\n'.format(formatedData[commit][0]))
-            bugReport.write('description: {}\n\n'.format(formatedData[commit][1]))
-            bugReport.write('classification: {}\n\n'.format(formatedData[commit][2]))
-            bugReport.write('keywords: {}\n\n'.format(formatedData[commit][3]))
-            bugReport.write('severity{}\n\n'.format(formatedData[commit][4]))
-            bugReport.write('links{}\n\n'.format(formatedData[commit][17]))
-            bugReport.write('bug: \n\n')
-            bugReport.write('  phase: {}\n\n'.format(formatedData[commit][5]))
-            bugReport.write('  specifity: {}\n\n'.format(formatedData[commit][6]))
-            bugReport.write('  architectural-location: {}\n\n'.format(cellNotPopulated)) # Using empty cell
-            bugReport.write('  application{}\n\n'.format(cellNotPopulated)) # Using empty cell
-            bugReport.write('  task: {}\n\n'.format(cellNotPopulated)) # Using empty cell
-            bugReport.write('  subsystem: {}\n\n'.format(cellNotPopulated)) # Using empty cell
-            bugReport.write('  package: {}\n\n'.format(cellNotPopulated)) # Using empty cell
-            bugReport.write('  language: {}\n\n'.format(formatedData[commit][14]))
-            bugReport.write('  detected-by: {}\n\n'.format(formatedData[commit][7]))
-            bugReport.write('  reported-by: {}\n\n'.format(formatedData[commit][8]))
-            bugReport.write('  isssue: {}\n\n'.format(formatedData[commit][9]))
-            bugReport.write('  time-reported: {}\n\n'.format(formatedData[commit][10]))
-            bugReport.write('  reproducibility: {}\n\n'.format(cellNotPopulated))
-            bugReport.write('  trace: {}\n\n'.format(cellNotPopulated)) # Using empty cell
-            bugReport.write('fix: \n\n')
-            bugReport.write('  repo: {}\n\n'.format(cellNotPopulated)) # Using empty cell
-            bugReport.write('  hash: {}\n\n'.format(formatedData[commit][11]))
-            bugReport.write('  pull-request: {}\n\n'.format(formatedData[commit][12]))
-            bugReport.write('  license: {}\n\n'.format(cellNotPopulated)) # Using empty cell
-            bugReport.write('  fix-in: {}\n\n'.format(formatedData[commit][13]))
-            bugReport.write('  language: {}\n\n'.format(formatedData[commit][14]))
-            bugReport.write('  time: {}\n\n'.format(formatedData[commit][15]))
+        count = 0
+        with open('bugs/{}.bug'.format(formatedData[commit][21][0:6]), 'a') as bugReport:
+            for line in _format:
+
+                if 'bug:' in line:
+                    bugReport.write('bug: \n')
+                    continue
+                if 'fix:' in line:
+                    bugReport.write('fix: \n')
+                    continue
+                lineToWrite = line.replace('\n','') 
+                lineToWrite += str(' {}'.format(formatedData[commit][count]).replace('\n',''))
+                bugReport.write(lineToWrite)
+                bugReport.write('\n\n')
+                count += 1
+            count = 0
     print ('Done.')
 
 
